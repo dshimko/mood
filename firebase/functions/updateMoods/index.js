3f6066baf6fd7b4eb9508df67e163e5fa433cc68
Your Name <you@example.com>
2017-11-20 14:45:10 +0000

    adding firebase function

---
+++ firebase/functions/updateMoods/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// Keeps track of the length of the 'likes' child list in a separate property.
+exports.countlikechange = functions.database.ref('/rest/mood').onWrite(event => {
+  const collectionRef = event.data.ref.parent;
+  const countRef = collectionRef.parent.child('Current Mood');
+
+  // Return the promise from countRef.transaction() so our function 
+  // waits for this async event to complete before it exits.
+  return countRef.transaction(current => {
+    if (event.data.exists() && !event.data.previous.exists()) {
+      return (current || 0) + 1;
+    }
+    else if (!event.data.exists() && event.data.previous.exists()) {
+      return (current || 0) - 1;
+    }
+  }).then(() => {
+    console.log('Counter updated.');
+  });
+});
+
+// If the number of likes gets deleted, recount the number of likes
+exports.recountlikes = functions.database.ref('/rest/Current Mood').onWrite(event => {
+  if (!event.data.exists()) {
+    const counterRef = event.data.ref;
+    const collectionRef = counterRef.parent.child('mood');
+    
+    // Return the promise from counterRef.set() so our function 
+    // waits for this async event to complete before it exits.
+    return collectionRef.once('value')
+        .then(messagesData => counterRef.set(messagesData.numChildren()));
+  }
+});
